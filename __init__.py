3979502fee419af86c49e04c9ca6790ad088545b
Andre Chatzistamatiou <a.chatzistamatiou@uni-due.de>
2019-08-21 22:10:37 +0200

    Use only functions in checks

--- __init__.py
+++ __init__.py
@@ -1,7 +1,1 @@
-#from segment.globals import globals
-#from segment.utils import utils
-#from segment.encoder import encoder
-#from segment.decoder import decoder
-#from segment.data import data
-#from segment.checks import checks
 from segment import globals, data, decoder, encoder, checks
--- checks/__init__.py
+++ checks/__init__.py
@@ -3,7 +3,9 @@ from segment.globals import training_bucket
 import segment.data as data
 
 # credentials
-assert 'credentials' in os.listdir(os.path.join(os.environ['HOME'], '.aws')), "Set up your credentials in ~/.aws/credentials (see boto3 docs)"
+def credentials():
+    assert 'credentials' in os.listdir(os.path.join(os.environ['HOME'], '.aws')), "Set up your credentials in ~/.aws/credentials (see boto3 docs)"
 
 # training bucket is available
-assert training_bucket in data.os.listdir(), "Training bucket not found. Set globals.training_bucket, example:  \"/my_bucket_name\""
+def training_bucket():
+    assert training_bucket in data.os.listdir(), "Training bucket not found. Set globals.training_bucket, example:  \"/my_bucket_name\""
