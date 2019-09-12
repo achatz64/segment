16c73d416514cfd9299dbfcd7566286d72ce85d0
Andre Chatzistamatiou <a.chatzistamatiou@uni-due.de>
2019-09-12 22:51:26 +0200

    Attach decoder to encoder. Simpler baseline decoder.

--- decoder/setup_try.py
+++ decoder/setup_try.py
@@ -1,4 +1,4 @@
-from tensorflow.python.keras.layers import SeparableConv2D, BatchNormalization, Input, Concatenate, Lambda
+from tensorflow.python.keras.layers import SeparableConv2D, BatchNormalization, Input, Concatenate, Lambda, Conv2D
 from tensorflow.python.keras.engine.training import Model as ModelType
 from tensorflow.python.framework.ops import Tensor as TensorType
 from tensorflow.image import resize
@@ -53,8 +53,7 @@ def decoder_from_skeleton(skeleton: List[Dict[str, Union[int, Callable[[TensorTy
 
         # resizing for concatenation (unclear why wrapping in Lambda in necessary in 2.0 but not in 1.14)
         # f = Lambda(lambda x: tf.compat.v1.image.resize(f, tf.compat.v1.shape(x)[1:3]))(input_here)
-        f = Lambda(lambda x: resize(f, shape(x)[1:3]))(input_here)
-
+        f = Lambda(lambda x: resize(x[1], shape(x[0])[1:3]))([input_here, f])
 
         concat = Concatenate()([f, from_encoder])
 
@@ -64,10 +63,80 @@ def decoder_from_skeleton(skeleton: List[Dict[str, Union[int, Callable[[TensorTy
 
     # resizing to image dimensions (unclear why wrapping in Lambda in necessary in 2.0 but not in 1.14)
     #output = Lambda(lambda x: tf.compat.v1.image.resize(f, x[0]))(image_shape_layer)
-    output = Lambda(lambda x: resize(f, x[0]))(image_shape_layer)
+    output = Lambda(lambda x: resize(x[1], x[0][0]))([image_shape_layer, f])
 
     return Model((inputs, image_shape_layer), output)
 
+
+def baseline_skeleton(channels: List[Dict[str, int]],
+                      batch_normalization=True,
+                      kernel=(3, 3)):
+    """
+    Constructs a primitive decoder skeleton.
+
+    channels is of the form:
+    [{'e-channel': x, 'd-channel': y}, {'e-channel': xx, 'd-channel': yy}, ...]
+    """
+
+    assert len(channels) > 0, "Empty list of channels!"
+
+    def squeeze(to_channels, activation="relu"):
+
+        def out(tensor):
+            tensor = SeparableConv2D(to_channels, kernel, padding="same", activation=activation)(tensor)
+
+            if batch_normalization:
+                tensor = BatchNormalization()(tensor)
+
+            return tensor
+
+        return out
+
+    skeleton = [{"e/d-conn": squeeze(channels[0]['d-channel']),
+                 "channels": channels[0]['e-channel']}]
+
+    for channel_pair in channels[1:-1]:
+        new = {"channels": channel_pair["e-channel"],
+               "e-conn": Lambda(lambda x: x),
+               "d-conn": squeeze(channel_pair["d-channel"])}
+        skeleton.append(new)
+
+    last = {"channels": channels[-1]["e-channel"],
+            "e-conn": Lambda(lambda x: x),
+            "d-conn": squeeze(channels[-1]["d-channel"], activation='softmax')}
+
+    skeleton.append(last)
+
+    return skeleton
+
+def zero_skeleton(channels : List[int]):
+
+    def edconn(f: TensorType):
+        for c in channels[1:]:
+            f = Conv2D(c, (1, 1), activation='relu')(f)
+        return f
+
+    return [{"channels": channels[0],
+             "e/d-conn": edconn}]
+
+
+def attach(encoder: ModelType,
+           decoder: ModelType,
+           use_layers: List[int]):
+
+    # large layers first
+    use_layers = sorted(use_layers, key=lambda x: -x)
+
+    inputs = encoder.input
+    layers = [encoder.layers[i] for i in use_layers]
+
+    for l in layers:
+        print(l.output_shape)
+
+    out_decoder = decoder(
+        [l.output for l in layers] + [Lambda(lambda x: tf.map_fn(lambda y: shape(y)[0:2], x, dtype=tf.int32))(inputs)])
+
+    return Model(inputs, out_decoder)
 
 
 
